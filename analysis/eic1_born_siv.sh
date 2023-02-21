3460dabe2e9df8acf782e067c762ae4ae494e036
Shuo <shuojia@jlab.org>
2023-02-21 12:46:47 -0500

    new file:   analysis/eic1_born_siv.sh 	new file:   analysis/eic1_born_unpol.sh 	new file:   analysis/eic1_rc_siv.sh 	new file:   analysis/eic1_rc_unpol.sh 	modified:   analysis/fac_fine.sh

---
+++ analysis/eic1_born_siv.sh
@@ -0,0 +1,6 @@
+echo "Using numbers in increase_grids.txt"
+while IFS="" read -r p || [ -n "$p" ]
+do
+  echo "${p}"
+  python main.py born siv 140 0.001148 0.5 3 ${p} >>results/Fac-results/Fine/EIC_1_siv_born.txt
+done < analysis/increase_grids.txt
