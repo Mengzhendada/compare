4558b6c23f776752ae25e695ee6e27482da0b495
Shuo Jia <shuojia@Shuos-MacBook-Air.local>
2023-03-02 12:21:09 -0500

    modified:   Traditional_Factorized_compare_fine.ipynb 	modified:   Traditional_factorized_compare.ipynb 	modified:   Unpol_proposal.ipynb 	modified:   analysis/JLab2.sh 	modified:   analysis/JLab3.sh 	modified:   analysis/eic1.sh 	modified:   analysis/eic2.sh

--- analysis/JLab3.sh
+++ analysis/JLab3.sh
@@ -1,7 +1,7 @@
-python main.py rc col 6.68 0.48 0.375 3.873 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_col_rc.txt
-python main.py born col 6.68 0.48 0.375 3.873 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_col_bn.txt
-python main.py rc unpol 6.68 0.48 0.375 3.873 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_unpol_rc.txt
-python main.py born unpol 6.68 0.48 0.375 3.873 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_unpol_bn.txt
-python main.py rc siv 6.68 0.48 0.375 3.873 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_siv_rc.txt
-python main.py born siv 6.68 0.48 0.375 3.873 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_siv_bn.txt
+python main.py rc col 6.71096567 0.48 0.375 3.872983346 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_col_rc.txt
+python main.py born col 6.71096567 0.48 0.375 3.872983346 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_col_bn.txt
+python main.py rc unpol 6.71096567 0.48 0.375 3.872983346 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_unpol_rc.txt
+python main.py born unpol 6.71096567 0.48 0.375 3.872983346 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_unpol_bn.txt
+python main.py rc siv 6.71096567 0.48 0.375 3.872983346 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_siv_rc.txt
+python main.py born siv 6.71096567 0.48 0.375 3.872983346 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/JLab_3_siv_bn.txt
 
--- analysis/eic1.sh
+++ analysis/eic1.sh
@@ -1,7 +1,7 @@
-python main.py rc col 140 0.001148 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_col_rc.txt
-python main.py born col 140 0.001148 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_col_bn.txt
-python main.py rc unpol 140 0.001148 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_unpol_rc.txt
-python main.py born unpol 140 0.001148 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_unpol_bn.txt
-python main.py rc siv 140 0.001148 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_siv_rc.txt
-python main.py born siv 140 0.001148 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_siv_bn.txt
+python main.py rc col 140 0.00114795918 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_col_rc.txt
+python main.py born col 140 0.00114795918 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_col_bn.txt
+python main.py rc unpol 140 0.00114795918 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_unpol_rc.txt
+python main.py born unpol 140 0.00114795918 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_unpol_bn.txt
+python main.py rc siv 140 0.00114795918 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_siv_rc.txt
+python main.py born siv 140 0.00114795918 0.5 3 [0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6]&>results/Fac-results/EIC_1_siv_bn.txt
 
