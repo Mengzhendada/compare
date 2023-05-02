419f206aeba45fa310e3ef167da1dd72626321b9
Shuo <shuojia@jlab.org>
2023-05-01 21:24:24 -0400

    modified:   Duane_input_lowQ2.txt 	new file:   Unpol_1xs.txt 	new file:   Unpol_1xs_notlowQ2x.txt 	modified:   check_t.sh

--- analysis/Unpol/check_t.sh
+++ analysis/Unpol/check_t.sh
@@ -8,9 +8,9 @@ do
   #./get_cross_section 0 5.75 U U 0.2 0.93 0.5 0.16 ${f1} 1 0.01>>out.txt
   #./get_cross_section 0 5.75 U U 0.2 0.93 0.5 0.4 ${f1} 1 0.01>>out.txt
   #./get_cross_section 0 5.75 U U 0.2 0.93 0.5 0.632 ${f1} 1 0.01>>out.txt
-  ./get_cross_section 0 11.0 U L 0.4 0.48 0.5 0.16 ${f1} 1 0.01>>out.txt
-  ./get_cross_section 0 11.0 U L 0.4 0.48 0.5 0.4 ${f1} 1 0.01>>out.txt
-  ./get_cross_section 0 11.0 U L 0.4 0.48 0.5 0.632 ${f1} 1 0.01>>out.txt
+  ./get_cross_section 0 11.0 U U 0.4 0.48 0.5 0.16 ${f1} 1 0.01>>out.txt
+  ./get_cross_section 0 11.0 U U 0.4 0.48 0.5 0.4 ${f1} 1 0.01>>out.txt
+  ./get_cross_section 0 11.0 U U 0.4 0.48 0.5 0.632 ${f1} 1 0.01>>out.txt
 done < compare/analysis/Unpol/phih.txt
 #./get_cross_section 0 5.75 U U 0.2 0.93 0.5 0.16 3 1 0.01>>out.txt
 #./get_cross_section 0 5.75 U U 0.2 0.93 0.5 0.4 3 1 0.01>>out.txt
