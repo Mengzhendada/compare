2e3f1b67a580904efae166afc580acfe93a2f202
Shuo <shuojia@jlab.org>
2023-05-22 17:26:37 -0400

    new file:   test/FuuT_cos_out.txt 	new file:   test/FuuT_out.txt 	new file:   test/all_out.txt 	new file:   test/check_t.sh 	new file:   test/check_t_new.sh 	new file:   test/check_t_new2.sh 	new file:   test/out_readme.txt 	new file:   test/phih.txt

---
+++ analysis/test/check_t.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+module use /apps/modulefiles
+cd /w/halla-scshelf2102/solid/shuo/sidis
+source setup_sidis
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64:$LD_LIBRARY_PATH
+while IFS="," read -r f1
+do
+  ./get_cross_section 0 10.6 U U 0.31 0.405 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/all_out.txt
+  ./get_cross_section 0 10.6 U U 0.31 0.405 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/all_out.txt
+  ./get_cross_section 0 10.6 U U 0.31 0.405 0.4 1 ${f1} 1 0.01>>compare/analysis/test/all_out.txt
+  ./get_cross_section 0 10.6 U U 0.31 0.6 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/all_out.txt
+  ./get_cross_section 0 10.6 U U 0.31 0.6 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/all_out.txt
+  ./get_cross_section 0 10.6 U U 0.31 0.6 0.4 1 ${f1} 1 0.01>>compare/analysis/test/all_out.txt
+done < compare/analysis/test/phih.txt
+
---
+++ analysis/test/out_readme.txt
@@ -0,0 +1,4 @@
+#We do not have good FuuL expression for now. So the results are without FuuL terms
+*all_out.txt is for FuuT+Fuucosphi+Fuucos2phi
+*FuuT_out.txt is for FuuT only
+*FuuT_cos_out.txt is for FuuT+Fuucosphi
