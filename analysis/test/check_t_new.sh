76392424f55282eafb9c1cfc27772cd1c50d3711
Shuo <shuojia@jlab.org>
2023-05-24 16:45:20 -0400

    new file:   test/FuuT_cos_out_6.txt 	new file:   test/FuuT_cos_out_7.txt 	new file:   test/FuuT_cos_out_8.txt 	new file:   test/FuuT_out_6.txt 	new file:   test/FuuT_out_7.txt 	new file:   test/FuuT_out_8.txt 	new file:   test/all_out_6.txt 	new file:   test/all_out_7.txt 	new file:   test/all_out_8.txt 	modified:   test/check_t.sh 	modified:   test/check_t_new.sh 	modified:   test/check_t_new2.sh

--- analysis/test/check_t_new.sh
+++ analysis/test/check_t_new.sh
@@ -5,11 +5,29 @@ source setup_sidis
 export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64:$LD_LIBRARY_PATH
 while IFS="," read -r f1
 do
-  ./get_cross_section 0 10.6 U U 0.31 0.405 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
-  ./get_cross_section 0 10.6 U U 0.31 0.405 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
-  ./get_cross_section 0 10.6 U U 0.31 0.405 0.4 1 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
-  ./get_cross_section 0 10.6 U U 0.31 0.6 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
-  ./get_cross_section 0 10.6 U U 0.31 0.6 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
-  ./get_cross_section 0 10.6 U U 0.31 0.6 0.4 1 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
+  #./get_cross_section 0 10.6 U U 0.31 0.405 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
+  #./get_cross_section 0 10.6 U U 0.31 0.405 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
+  #./get_cross_section 0 10.6 U U 0.31 0.405 0.4 1 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
+  #./get_cross_section 0 10.6 U U 0.31 0.6 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
+  #./get_cross_section 0 10.6 U U 0.31 0.6 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
+  #./get_cross_section 0 10.6 U U 0.31 0.6 0.4 1 ${f1} 1 0.01>>compare/analysis/test/FuuT_out.txt
+  ./get_cross_section 0 6.535 U U 0.31 0.6576 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_6.txt
+  ./get_cross_section 0 6.535 U U 0.31 0.6576 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_6.txt
+  ./get_cross_section 0 6.535 U U 0.31 0.6576 0.4 1 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_6.txt
+  ./get_cross_section 0 6.535 U U 0.31 0.9733 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_6.txt
+  ./get_cross_section 0 6.535 U U 0.31 0.9733 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_6.txt
+  ./get_cross_section 0 6.535 U U 0.31 0.9733 0.4 1 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_6.txt
+  ./get_cross_section 0 7.546 U U 0.31 0.5695 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_7.txt
+  ./get_cross_section 0 7.546 U U 0.31 0.5695 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_7.txt
+  ./get_cross_section 0 7.546 U U 0.31 0.5695 0.4 1 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_7.txt
+  ./get_cross_section 0 7.546 U U 0.31 0.8429 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_7.txt
+  ./get_cross_section 0 7.546 U U 0.31 0.8429 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_7.txt
+  ./get_cross_section 0 7.546 U U 0.31 0.8429 0.4 1 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_7.txt
+  ./get_cross_section 0 8.4 U U 0.31 0.5116 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_8.txt
+  ./get_cross_section 0 8.4 U U 0.31 0.5116 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_8.txt
+  ./get_cross_section 0 8.4 U U 0.31 0.5116 0.4 1 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_8.txt
+  ./get_cross_section 0 8.4 U U 0.31 0.7572 0.4 0.00625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_8.txt
+  ./get_cross_section 0 8.4 U U 0.31 0.7572 0.4 0.5625 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_8.txt
+  ./get_cross_section 0 8.4 U U 0.31 0.7572 0.4 1 ${f1} 1 0.01>>compare/analysis/test/FuuT_out_8.txt
 done < compare/analysis/test/phih.txt
 
